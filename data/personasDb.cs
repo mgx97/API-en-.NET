e61d9c40e0545ffa0383551ae796a7381adc5698
mgx97 <147671742+mgx97@users.noreply.github.com>
2023-10-27 00:19:16 -0300

    Tarea Desarrollo Api

---
+++ data/personasDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.models;
+
+namespace WebApplication1.data
+{
+    public class personasDb : DbContext
+    {
+
+        public personasDb(DbContextOptions<personasDb> options) : base(options)
+        { 
+        
+        
+        }
+
+        public DbSet<persona> personas => Set<persona>();
+    }
+}
---
+++ models/ciudad.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.models
+{
+    public class ciudad
+    {
+        public int Id { get; set; }
+
+        public string NombreCiudad { get; set; }
+    }
+}
