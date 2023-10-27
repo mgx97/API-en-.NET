6df8858039686f1796f27d8daf30208781c409be
mgx97 <147671742+mgx97@users.noreply.github.com>
2023-10-26 23:50:56 -0300

    Add files via upload
    Parcial II

---
+++ controller/CiudadController.cs
@@ -0,0 +1,79 @@
+﻿using API.data;
+using API.models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.controllers
+{
+    public class CiudadController : Controller
+    {
+        private readonly apiDb _dbContext;
+
+        public CiudadController(apiDb dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Ciudad ciudad)
+        {
+            _dbContext.Ciudades.Add(ciudad);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            Ciudad ciudad = _dbContext.Ciudades.Find(id);
+
+            if (ciudad is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ciudad);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_dbContext.Ciudades.ToList());
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id, [FromBody] Ciudad ciudad)
+        {
+            Ciudad ciudadActual = _dbContext.Ciudades.Find(id);
+
+            if (ciudadActual is null)
+            {
+                return NotFound();
+            }
+
+            ciudadActual.NombreCiudad = ciudad.NombreCiudad;
+            ciudadActual.Departamento = ciudad.Departamento;
+            ciudadActual.CodigoPostal = ciudad.CodigoPostal;
+
+            _dbContext.SaveChanges();
+
+            return Ok(ciudadActual);
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            Ciudad ciudad = _dbContext.Ciudades.Find(id);
+
+            if (ciudad is null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Ciudades.Remove(ciudad);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ models/Cuenta.cs
@@ -0,0 +1,17 @@
+﻿namespace API.models
+{
+    public class Cuenta
+    {
+        public int Id { get; set; }
+        public int IdCliente { get; set; }
+        public string NroCuenta { get; set; }
+        public DateTime FechaAlta { get; set; }
+        public string TipoCuenta { get; set; }
+        public string Estado { get; set; }
+        public decimal Saldo { get; set; }
+        public string NroContrato { get; set; }
+        public decimal CostoMantenimiento { get; set; }
+        public decimal PromedioAcreditacion { get; set; }
+        public string Moneda { get; set; }
+    }
+}
