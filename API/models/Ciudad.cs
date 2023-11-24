bf99b7d6ad8841c79b5a81587e734c0f253fbeb3
mgx97 <147671742+mgx97@users.noreply.github.com>
2023-11-23 22:28:24 -0300

    TERCER EXAMEN PARCIAL

---
+++ API/models/Ciudad.cs
@@ -0,0 +1,10 @@
+﻿namespace API.models
+{
+    public class Ciudad
+    {
+        public int Id { get; set; }
+        public string NombreCiudad { get; set; }
+        public string Departamento { get; set; }
+        public string CodigoPostal { get; set; }
+    }
+}
---
+++ API/models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace API.models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public int IdPersona { get; set; }
+        public string Calificacion { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Estado { get; set; }
+  
+
+    }
+}
